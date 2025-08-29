052fd673db756a3fcf4a20b30df99e306562ff41
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2025-08-29 08:51:10 -0300

    refactor: put all js at main.js

--- main.js
+++ main.js
@@ -68,5 +68,24 @@ function initContactForm() {
     });
 }
 
+const scrollProgress = document.getElementById("progress");
+
+function updateBackToTopVisibility() {
+  const y = window.scrollY || document.documentElement.scrollTop || 0;
+  scrollProgress.style.display = y > 100 ? "flex" : "none";
+}
+
+if (scrollProgress) {
+  scrollProgress.addEventListener("click", () => {
+    window.scrollTo({ top: 0});
+  });
+
+  window.addEventListener("load", updateBackToTopVisibility);
+  window.addEventListener("resize", updateBackToTopVisibility);
+  window.addEventListener("scroll", updateBackToTopVisibility, { passive: true });
+
+  updateBackToTopVisibility();
+}
+
 
 
